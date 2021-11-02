b08f308a925a94d29256dc7264719b9c868150fc
frederik <frede.madsen3@gmail.com>
2021-11-02 22:45:23 +0100

    chapter 12 exercises 1,2,3,4

--- chapter-012/records/lib/records.ex
+++ chapter-012/records/lib/records.ex
@@ -1,4 +1,9 @@
 defmodule Records do
+  require Records.Tower
+  alias Records.Tower, as: Tower
+  """
+  Example 12.1 Examples of records
+  """
   defmodule Planemo do
     require Record
     Record.defrecord :planemo, [name: :nil, gravity: 0, diameter: 0, distance_from_sun: 0]
@@ -7,5 +12,33 @@ defmodule Records do
   defmodule Tower do
     require Record
     Record.defrecord :tower, Tower, [location: "", height: 20, planemo: :earth, name: ""]
+  end
+  """
+  Example 12.2 Using records in functions
+  """
+  def fall_velocity(t = Tower.tower()) do
+    fall_velocity(Tower.tower(t, :planemo), Tower.tower(t, :height))
+  end
+
+  """
+  Example 12.3 Pattern matching with records
+  """
+  def fall_velocity(Tower.tower(planemo: planemo, height: height)) do
+    fall_velocity(planemo, height)
+  end
+
+  @spec fall_velocity(atom(), number()) :: float()
+  def fall_velocity(:earth, distance) do
+    :math.sqrt(2 * 9.8 * distance)
+  end
+
+  @spec fall_velocity(atom(), number()) :: float()
+  def fall_velocity(:moon, distance) do
+    :math.sqrt(2 * 1.6 * distance)
+  end
+
+  @spec fall_velocity(atom(), number()) :: float()
+  def fall_velocity(:mars, distance) do
+    :math.sqrt(2 * 3.71 * distance)
   end
 end
